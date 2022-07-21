18f64aaf4ab3914dbd4bbf4078980b9b41d68828
Boshra Jaber <boshrajaber94@gmail.com>
2022-07-21 23:44:06 +0300

    Cleaning up

--- bin/pull-challenge.sh
+++ bin/pull-challenge.sh
@@ -2,7 +2,5 @@
 
 echo "pulling Code Challenge $1"
 
-# mkdir code-challenges/challenges-$1
-
 `which curl` -s https://ltuc.github.io/new-prep-challenges-content/day-$1/Challenge$1.js > ./code-challenges/challenges-$1.js
 `which curl` -s https://ltuc.github.io/new-prep-challenges-content/day-$1/Challenge$1.test.js > ./test/challenges-$1.test.js
